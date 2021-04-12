fa7b1a0b7e06bd7d8e06b790aa2eb0a3d42cba28
JestorNogueiro <jestornogueiro789@gmail.com>
2021-04-12 12:43:46 +0530

    Adding Images

--- src/Pages.js
+++ src/Pages.js
@@ -4,6 +4,7 @@ const Pages = () => {
   return (
     <div>
       <section className="page__section">
+        {/* Images to display on  */}
         <div className="pic1">
           <img src="https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=602&q=80" />
           Burger
